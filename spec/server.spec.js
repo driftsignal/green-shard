b402e3bbff9a1be13b69a9d47d59dce8441235f0
driftsignal <27819662+driftsignal@users.noreply.github.com>
2018-06-23 16:39:19 -0400

    test inconsistancy

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -4,7 +4,7 @@ const baseUrl = 'http://localhost:4000/'
 
 describe('Web Server', () => {
   ['graphql', '/'].forEach(v => {
-    it(`${v} returns status code 200`, () => {
+    it(v + 'returns status code 200', () => {
       request.get(baseUrl + v, (err, res, body) =>
                 expect(res.statusCode).toBe(200))
     })
